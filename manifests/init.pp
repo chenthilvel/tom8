88dcae5459f0c8f170a6f24d411717526c6fd74f
Chenthil <chenthilvel@users.noreply.github.com>
2017-03-14 21:35:47 +0530

    fix manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,66 +1,58 @@
-class tomcat8
-{
+class tom8 {
+  package { 'redhat-lsb.x86_64': ensure => present, }
 
-#user { 'tomcat8' :
-#    ensure  => 'present',
-#    home    => '/usr/tomcat8',
-#}
-
-package { "redhat-lsb.x86_64" :
-    ensure => present,
-  }
-file { "/usr/tomcat8" : 
-    ensure => directory,
-    owner => 'webuser',
-    group => 'webuser',
+  file { '/usr/tomcat8':
+    ensure  => directory,
+    owner   => 'webuser',
+    group   => 'webuser',
     recurse => true,
     require => Class['base'],
-}
+  }
 
-file { '/root/apache-tomcat-8.0.32.tar.gz' :
-      owner => "root",
-      group => "root",
-      ensure => "present",
-      source => "puppet:///modules/tomcat8/tomcat8.tar.gz",
-      notify => Exec['untartomcat8'],
-}
+  file { '/root/apache-tomcat-8.0.32.tar.gz':
+    owner  => 'root',
+    group  => 'root',
+    ensure => 'present',
+    source => 'puppet:///modules/tom8/tomcat8.tar.gz',
+    notify => Exec['untartomcat8'],
+  }
 
-exec { 'untartomcat8' :
-  command     => "/bin/tar -C / -xvzf /root/apache-tomcat-8.0.32.tar.gz",
-  cwd         => '/root/',
-  refreshonly => true,
-  require => File['/root/apache-tomcat-8.0.32.tar.gz'],
-  notify  => File['/usr/tomcat8'],
-}
+  exec { 'untartomcat8':
+    command     => '/bin/tar -C / -xvzf /root/apache-tomcat-8.0.32.tar.gz',
+    cwd         => '/root/',
+    refreshonly => true,
+    require     => File['/root/apache-tomcat-8.0.32.tar.gz'],
+    notify      => File['/usr/tomcat8'],
+  }
 
-file { "/usr/tomcat8/webapps" : 
-    ensure => directory,
-    owner => 'webuser',
-    group => 'webuser',
-    mode => 0774,
+  file { '/usr/tomcat8/webapps':
+    ensure  => directory,
+    owner   => 'webuser',
+    group   => 'webuser',
+    mode    => 0774,
     require => Class['base']
-}
+  }
 
-file { "/var/log/tomcat8" : 
-    ensure => directory,
-    owner => 'webuser',
-    group => 'webuser',
-    mode => 0774,
+  file { '/var/log/tomcat8':
+    ensure  => directory,
+    owner   => 'webuser',
+    group   => 'webuser',
+    mode    => 0774,
     require => Class['base']
-}
+  }
 
-file { '/usr/sbin/dtomcat8' :
-    content => template('tomcat8/dtomcat8.erb'),
+  file { '/usr/sbin/dtomcat8':
+    content => template('tom8/dtomcat8.erb'),
     owner   => root,
     group   => root,
     mode    => 755,
-}
+  }
 
-file { '/etc/init.d/tomcat8' :
-    content => template('tomcat8/tomcat8_init.erb'),
+  file { '/etc/init.d/tomcat8':
+    content => template('tom8/tomcat8_init.erb'),
     owner   => root,
     group   => root,
     mode    => 755,
-}
+  }
 
 }
